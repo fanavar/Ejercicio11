a6d7a7a9ef9cd374899e2647acaea7aa6286d36b
Fabián <fanavar@alumnos.ubiobio.cl>
2023-06-28 20:46:05 -0400

    ejercicio 11 80/80

--- app/src/main/java/cl/awakelab/ejercicio11/MainActivity.java
+++ app/src/main/java/cl/awakelab/ejercicio11/MainActivity.java
@@ -3,12 +3,33 @@ package cl.awakelab.ejercicio11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import cl.awakelab.ejercicio11.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.buttonPlane.setOnClickListener(v -> Toast.makeText(this, "modo avión desactivado", Toast.LENGTH_SHORT).show());
+        binding.buttonUpdate.setOnClickListener(v -> Toast.makeText(this, "perfil actualizado", Toast.LENGTH_SHORT).show());
+        binding.buttonPicture.setOnClickListener(v -> Toast.makeText(this, "imagen guardada en galeria", Toast.LENGTH_SHORT).show());
+        binding.buttonUlocked.setOnClickListener(v -> Toast.makeText(this, "nivel 5 desbloqueado", Toast.LENGTH_SHORT).show());
+        binding.buttonLevel.setOnClickListener(v -> Toast.makeText(this, "nivel 4 completado", Toast.LENGTH_SHORT).show());
+        binding.buttonMusic.setOnClickListener(v -> Toast.makeText(this, "musica pausada", Toast.LENGTH_SHORT).show());
+
     }
+
+
+
+
+
+
+
+
 }
